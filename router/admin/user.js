4d9043e9f77135d61d94bbe48490e82f7a205a71
hushuaibo <979587457@qq.com>
2019-09-22 18:06:09 +0800

    用户添加（有问题）

--- router/admin/user.js
+++ router/admin/user.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const DB = require('./../../modules/db.js');
-const router = express.Router();
+let express = require('express');
+let router = express.Router();
+let DB = require('./../../modules/db.js');
 
 router.get('/userList',function (req,res) {
     DB.find('user', {}, function (err, users) {
@@ -16,6 +16,41 @@ router.get('/userList',function (req,res) {
             users:users
         });
     });
+});
+
+router.post('/userAdd',function (req,res) {
+    const USERNAME = req.query.username;
+    const PASSWORLD = req.query.password;
+    const AGE = req.query.age;
+    const GENDER = req.query.gender;
+    const BIRTHDAY = req.query.birthday;
+    const PICTURE = req.query.picture;
+    const HOMETOWN = req.query.hometown;
+    const PHONE = req.query.phone;
+    const QQ = req.query.qq;
+    DB.insert('user',{
+        UserName:USERNAME,
+        PassWorld:PASSWORLD,
+        Age:AGE,
+        Gender:GENDER,
+        Birthday:BIRTHDAY,
+        Picture:PICTURE,
+        Hometown:HOMETOWN,
+        Phone:PHONE,
+        QQ:QQ
+    },function (err,data) {
+        if(err){
+            res.send({
+                code : 0 ,
+                msg : '添加失败'
+            });
+            return
+        }
+        res.send({
+            code : 1 ,
+            msg : '添加成功'
+        })
+    })
 });
 
 module.exports = router;
